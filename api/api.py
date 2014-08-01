962627531125983f472ec7a43366a6ca61584010
Zaymar <isaiahmartin@unknown84383544b6fa.gateway.2wire.net>
2014-07-31 23:37:16 -0700

    Fixed er up

--- api/api.py
+++ api/api.py
@@ -11,8 +11,9 @@ def info():
 	global info
 	global data
 	global scripture
-	I1 = request.form['EmailInput1'] + "<br><br>"
-	data = [{'Email':'%s'%(I1)}]
+	I1 = request.form['EmailInput1'] 
+	I2 = request.form['Username'] 
+	data = [{'Email':'%s'%(I1)}, {'Student_Name':'%s'%(I2)}]
 	info = json.dumps(data)
 	return  redirect("http://localhost:8888/Capacity/Basic_Information.html", code=302)
 
@@ -21,14 +22,13 @@ data = data
 def info2():
 	global info
 	global data
-	I1 = request.form['focus'] + '<br>'
-	I2 = request.form['Username'] + '<br>'
-	I3 = request.form['Birth'] + '<br>'
-	I4 = request.form['Class'] + '<br>'
-	I5 = request.form['UPA']  + '<br>'
-	I6 = request.form['GPA'] + '<br>'
-	I7 = request.form['CSH'] + '<br>'
-	data = data + [{'Focus':'%s'%(I1)}, {'Student_Name':'%s'%(I2)}, {'DOB':'%s'%(I3)}, {'Class':'%s'%(I4)}, {'GPA':'%s'%(I5)}, {'GPA_Goal':'%s'%(I6)}, {'Community_Service_Goal':'%s'%(I7)}]
+	I1 = request.form['focus'] 
+	I3 = request.form['Birth'] 
+	I4 = request.form['Class'] 
+	I5 = request.form['UPA']  
+	I6 = request.form['GPA'] 
+	I7 = request.form['CSH'] 
+	data = data + [{'Focus':'%s'%(I1)}, {'DOB':'%s'%(I3)}, {'Class':'%s'%(I4)}, {'GPA':'%s'%(I5)}, {'GPA_Goal':'%s'%(I6)}, {'Community_Service_Goal':'%s'%(I7)}]
 	info = json.dumps(data, sort_keys=True)
 	return redirect("http://localhost:8888/Capacity/Student_Page.html", code=302)
 
@@ -36,18 +36,18 @@ def info2():
 def info3():
 	global info
 	global data
-	I1 = request.form['act1'] + "<br>"
-	I2 = request.form['act2'] + "<br>"
-	I3 = request.form['act3'] + "<br>"
-	I3 = request.form['WG1'] + "<br>"
-	I4 = request.form['WG2'] + "<br>"
-	I5 = request.form['WG3'] + "<br>"
-	I6 = request.form['MG1'] + "<br>"
-	I7 = request.form['MG2'] + "<br>"
-	I8 = request.form['MG3'] + "<br>"
-	I9 = request.form['YG1'] + "<br>"
-	I10 = request.form['YG2'] + "<br>"
-	I11 = request.form['YG3'] + "<br>"
+	I1 = request.form['act1'] 
+	I2 = request.form['act2'] 
+	I3 = request.form['act3'] 
+	I3 = request.form['WG1'] 
+	I4 = request.form['WG2'] 
+	I5 = request.form['WG3'] 
+	I6 = request.form['MG1'] 
+	I7 = request.form['MG2'] 
+	I8 = request.form['MG3'] 
+	I9 = request.form['YG1'] 
+	I10 = request.form['YG2'] 
+	I11 = request.form['YG3'] 
 
 	data = data + [{'Extra_Curricular_1':'%s'%(I1)}, {'Extra_Curricular_2':'%s'%(I2)}, {'Week_Goal_1':'%s'%(I3)}, {'Week_Goal_2':'%s'%(I4)}, {'Week_Goal_3':'%s'%(I5)},{'Month_Goal_1':'%s'%(I6)}, {'Month_Goal_2':'%s'%(I7)}, {'Month_Goal_3':'%s'%(I8)},{'Year_Goal_1':'%s'%(I9)}, {'Year_Goal_2':'%s'%(I10)}, {'Year_Goal_3':'%s'%(I11)}]
 	info = json.dumps(data, sort_keys=True)
@@ -57,20 +57,20 @@ def info3():
 def info4():
 	global info
 	global data
-	I1 = request.form['Date'] + "<br>"
-	I2 = request.form['Duties'] + "<br>"
-	I3 = request.form['Time'] + "<br>"
-	I4 = request.form['Organization'] + "<br>"
-	I5 = request.form['OrgLead'] + "<br>"
-	I6 = request.form['Wk1'] + "<br>"
-	I7 = request.form['Wk2'] + "<br>"
-	I8 = request.form['Wk3'] + "<br>"
-	I9 = request.form['M1'] + "<br>"
-	I10 = request.form['M2'] + "<br>"
-	I11 = request.form['M3'] + "<br>"
-	I12 = request.form['Y1'] + "<br>"
-	I13 = request.form['Y2'] + "<br>"
-	I14 = request.form['Y3'] + "<br>"
+	I1 = request.form['Date'] 
+	I2 = request.form['Duties'] 
+	I3 = request.form['Time'] 
+	I4 = request.form['Organization'] 
+	I5 = request.form['OrgLead'] 
+	I6 = request.form['Wk1'] 
+	I7 = request.form['Wk2'] 
+	I8 = request.form['Wk3'] 
+	I9 = request.form['M1'] 
+	I10 = request.form['M2'] 
+	I11 = request.form['M3'] 
+	I12 = request.form['Y1'] 
+	I13 = request.form['Y2'] 
+	I14 = request.form['Y3'] 
 
 	data = data + [{'Date':'%s'%(I1)}, {'Duties':'%s'%(I2)}, {'Time':'%s'%(I3)}, {'Organization':'%s'%(I4)}, {'OrgLead':'%s'%(I5)}, {'Wk1':'%s'%(I6)}, {'Wk2':'%s'%(I7)}, {'Wk3':'%s'%(I8)}, {'M1':'%s'%(I9)}, {'M2':'%s'%(I10)}, {'M3':'%s'%(I11)}, {'Y1':'%s'%(I12)}, {'Y2':'%s'%(I13)}, {'Y3':'%s'%(I14)}]
 	info = json.dumps(data, sort_keys=True)
@@ -80,8 +80,8 @@ def info4():
 def info5():
 	global info
 	global data
-	I1 = request.form['EmailInput2'] + "<br>"
-	I2 = request.form['name'] + "<br>"
+	I1 = request.form['EmailInput2'] 
+	I2 = request.form['name'] 
 	data = data + [{'Email':'%s'%(I1)}, {'Duties':'%s'%(I2)}]
 	info = json.dumps(data, sort_keys=True)
 	scripture.write(info);
