5b73a439c6600da3a4b2029c9f519a88f42cf93a
Zaymar <isaiahmartin@unknown84383544b6fa.gateway.2wire.net>
2014-08-05 23:08:09 -0700

    made multiple files but ima make em all on 1.

--- api/api.py
+++ api/api.py
@@ -3,67 +3,81 @@ import json
 from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
-scripture = open("info.txt", "a+")
-info = {}
-data = []
+def notify():
+	return '<title>Capacity</title>\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <!-- Bootstrap -->\
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">\
+    <script src="js/holder.js"></script>\
+    <div class="alert alert-dangerous alert-dismissible" role="alert">\
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
+  <strong>Sorry, but this email has already been taken.\
+</div>'
+
 @app.route('/submit', methods=['POST'])
-def info():
-	global info
-	global data
-	global scripture
+def Join_Now():
+	scripture = open("accounts.txt", "a+")
 	I1 = request.form['EmailInput1'] 
 	I2 = request.form['Nickname'] 
-	data = [{'Email':'%s'%(I1)}, {'Nickname':'%s'%(I2)}]
-	info = json.dumps(data)
-	return render_template("http://localhost:8888/Capacity/Basic_Information.html",EmailInput1=I1,Nickname=I2)
+	data = [{'email':(I1)}, {'nickname':(I2)}]
+	if str(I1) in open('accounts.txt').read():
+		return redirect("http://localhost:8888/Capacity/Notified_New_Beginnings.html#", code=302)
+	else:
+		info = json.dumps(data)
+		scripture.write(info);
+		scripture.close()
+		return redirect("http://localhost:8888/Capacity/Basic_Information.html", code=302)
 
-data = data
 @app.route('/submit2', methods=['POST'])
-def info2():
-	global info
-	global data
+def Basic_Info():
+	scripture = open("user_info.txt", "a+")
 	I1 = request.form['focus'] 
 	I3 = request.form['Birth'] 
 	I4 = request.form['Class'] 
 	I5 = request.form['UPA']  
 	I6 = request.form['GPA'] 
 	I7 = request.form['CSH'] 
-	data = data + [{'Focus':'%s'%(I1)}, {'DOB':'%s'%(I3)}, {'Class':'%s'%(I4)}, {'GPA':'%s'%(I5)}, {'GPA_Goal':'%s'%(I6)}, {'Community_Service_Goal':'%s'%(I7)}]
-	info = json.dumps(data, sort_keys=True)
+	data = [{'focus':(I1)}, {'dob':(I3)}, {'class':(I4)}, {'gpa':(I5)}, {'gpa_goal':(I6)}, {'community_service_goal':(I7)}]
+	info = json.dumps(data)
+	scripture.write(info);
+	scripture.close()
 	return redirect("http://localhost:8888/Capacity/Student_Page.html", code=302)
 
-data = data
 @app.route('/submit3',methods=['POST'])
-def info3():
-	global info
-	global data
+def Personal_Goals():
+	scripture = open("goals.txt", "a+")
+	I4 = request.form['WG1'] 
+	I5 = request.form['WG2'] 
+	I6 = request.form['WG3'] 
+	I7 = request.form['MG1'] 
+	I8 = request.form['MG2'] 
+	I9 = request.form['MG3'] 
+	I10 = request.form['YG1'] 
+	I11 = request.form['YG2'] 
+	I12 = request.form['YG3'] 
+
+	data = [{'week_goal_1':(I4)}, {'week_goal_2':(I5)}, {'week_goal_3':(I6)},{'month_goal_1':(I7)}, {'month_goal_2':(I8)}, {'month_goal_3':(I9)},{'year_goal_1':(I10)}, {'year_goal_2':(I11)}, {'Year_goal_3':(I12)}]
+	info = json.dumps(data)
+	scripture.write(info);
+	scripture.close()
+	return redirect("http://localhost:8888/Capacity/Community_Service.html", code=302)
+
+@app.route('/submit3.5',methods=['POST'])
+def ECs():
+	scripture = open("extra_curriculars.txt", "a+")
 	I1 = request.form['act1'] 
 	I2 = request.form['act2'] 
 	I3 = request.form['act3'] 
-	I3 = request.form['WG1'] 
-	I4 = request.form['WG2'] 
-	I5 = request.form['WG3'] 
-	I6 = request.form['MG1'] 
-	I7 = request.form['MG2'] 
-	I8 = request.form['MG3'] 
-	I9 = request.form['YG1'] 
-	I10 = request.form['YG2'] 
-	I11 = request.form['YG3'] 
 
-	data = data + [{'Extra_Curricular_1':'%s'%(I1)}, {'Extra_Curricular_2':'%s'%(I2)}, {'Week_Goal_1':'%s'%(I3)}, {'Week_Goal_2':'%s'%(I4)}, {'Week_Goal_3':'%s'%(I5)},{'Month_Goal_1':'%s'%(I6)}, {'Month_Goal_2':'%s'%(I7)}, {'Month_Goal_3':'%s'%(I8)},{'Year_Goal_1':'%s'%(I9)}, {'Year_Goal_2':'%s'%(I10)}, {'Year_Goal_3':'%s'%(I11)}]
-	info = json.dumps(data, sort_keys=True)
+	data = [{'extra_curricular_1':(I1)}, {'extra_curricular_2':(I2)}, {'extra_curricular_3':(I3)}]
+	info = json.dumps(data)
+	scripture.write(info);
+	scripture.close()
 	return redirect("http://localhost:8888/Capacity/Community_Service.html", code=302)
 
-data = data
 @app.route('/submit4',methods=['POST'])
-def info4():
-	global info
-	global data
-	I1 = request.form['Date'] 
-	I2 = request.form['Duties'] 
-	I3 = request.form['Time'] 
-	I4 = request.form['Organization'] 
-	I5 = request.form['OrgLead'] 
+def CS_Goals():
+	scripture = open("goals.txt", "a+")
 	I6 = request.form['Wk1'] 
 	I7 = request.form['Wk2'] 
 	I8 = request.form['Wk3'] 
@@ -74,22 +88,45 @@ def info4():
 	I13 = request.form['Y2'] 
 	I14 = request.form['Y3'] 
 
-	data = data + [{'Date':'%s'%(I1)}, {'Duties':'%s'%(I2)}, {'Time':'%s'%(I3)}, {'Organization':'%s'%(I4)}, {'OrgLead':'%s'%(I5)}, {'Wk1':'%s'%(I6)}, {'Wk2':'%s'%(I7)}, {'Wk3':'%s'%(I8)}, {'M1':'%s'%(I9)}, {'M2':'%s'%(I10)}, {'M3':'%s'%(I11)}, {'Y1':'%s'%(I12)}, {'Y2':'%s'%(I13)}, {'Y3':'%s'%(I14)}]
-	info = json.dumps(data, sort_keys=True)
-	return redirect("http://localhost:8888/Capacity/Sign_In.html", code=302)
-
-data = data
-@app.route('/submit5',methods=['POST'])
-def info5():
-	global info
-	global data
-	I1 = request.form['EmailInput2'] 
-	I2 = request.form['name'] 
-	data = data + [{'Email':'%s'%(I1)}, {'Nickname':'%s'%(I2)}]
-	info = json.dumps(data, sort_keys=True)
+	data = [{'wk1':(I6)}, {'wk2':(I7)}, {'wk3':(I8)}, {'m1':(I9)}, {'m2':(I10)}, {'m3':(I11)}, {'y1':(I12)}, {'y2':(I13)}, {'y3':(I14)}]
+	info = json.dumps(data)
 	scripture.write(info);
 	scripture.close()
-	return info
+	return redirect("http://localhost:8888/Capacity/Sign_In.html", code=302)
+
+@app.route('/submit4.5',methods=['POST'])
+def Comm_Serv():
+	scripture = open("community_service.txt", "a+")
+	I1 = request.form['Date'] 
+	I2 = request.form['Duties'] 
+	I3 = request.form['Time'] 
+	I4 = request.form['Organization'] 
+	I5 = request.form['OrgLead'] 
+
+	data = [{'date':(I1)}, {'duties':(I2)}, {'time':(I3)}, {'organization':(I4)}, {'org_lead':(I5)}]
+	info = json.dumps(data)
+	scripture.write(info);
+	scripture.close()
+	return redirect("http://localhost:8888/Capacity/Sign_In.html", code=302)
+
+@app.route('/submit5',methods=['POST'])
+def Sign_In():
+	scripture = open("accounts.txt", "a+")
+	I1 = request.form['EmailInput2'] 
+	I2 = request.form['name'] 
+	data = [{'email':(I1)}, {'nickname':(I2)}]
+	info = json.dumps(data)
+	scripture.write(info);
+	scripture.close()
+	return redirect("http://localhost:8888/Capacity/Student_Page.html", code=302)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
 
 if __name__ == "__main__":
 	app.debug = True
