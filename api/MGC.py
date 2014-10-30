d4ab87b13134d79ac092d75a45d7438b6fc41641
Zaymar <isaiahmartin@unknown84383544b6fa.attlocal.net>
2014-10-29 23:13:21 -0700

    Made ALOT of Changes
    Made ALOT of Changes

---
+++ api/MGC.py
@@ -0,0 +1,8 @@
+#MaxGPACalc aka MGC
+#add a feature that calculates the maximum gpa the student have by a given amount 
+#of time for the given gpa ( ex input = 3.0 by 2 semesters; max gpa=x )
+
+# current gpa =  2.56
+# semester = 5
+# logic:
+print	(2.56+4.0+4.0+4.0+4.0+4.0)/6
